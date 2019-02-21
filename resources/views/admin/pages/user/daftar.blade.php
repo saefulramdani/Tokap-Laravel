3ec18638bd5f6380780a39ec9af9918cd2d27829
saefulramdani <saefulramdani026gmail.com>
2019-02-21 10:00:13 +0700

    tokapsaeful

---
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -0,0 +1,90 @@
+@extends('admin.main')
+@section('title','User')
+@section('content')
+<h1>User</h1>
+<hr>
+
+@if(session('result') == 'success')
+<div class="alert alert-success alert-dismissible fade show">
+     <strong>Saved!</strong> Berhasil disimpan.
+     <button type="button" class="close" data-dismiss="alert">
+          &times;
+     </button>
+</div>
+@endif
+
+@if(session('result') == 'update')
+<div class="alert alert-success alert-dismissible fade show">
+     <strong>Updated!</strong> Berhasil diupdate.
+     <button type="button" class="close" data-dismiss="alert">
+          &times;
+     </button>
+</div>
+@endif
+
+<div class="row">
+	<div class="col-md-6 mb-3">
+		<a href="{{ route('admin.user.add') }}" class="btn btn-primary">[+] Tambah</a>
+	</div>
+
+    <div class="col-md-6 mb-3">
+    	 <form method="GET" action="{{ route('admin.user') }}">
+    	 	 <div class="input-group">
+    	 	 	 <input type="text" name="keyword"
+    	 	 	 value="{{ request('keyword') }}"
+    	 	 	 class="form-control">
+    	 	 	 <div class="input-group-append">
+    	 	 	 	 <button type="submit"
+    	 	 	 	 class="btn btn-primary">
+    	 	 	 	 	 Cari !
+    	 	 	 	 </button>
+    	 	 	 </div>
+    	 	 </div>
+    	 </form>
+    </div>
+
+</div>
+
+<table class="table table-striped mb-3">
+	<tr>
+		 <th>Name</th><th>Email</th><th>Akses</th><th>&nbsp;</th>
+	</tr>
+     @foreach($data as $dt)
+     <tr>
+     	<td>{{ $dt->name }}</td>
+     	<td>{{ $dt->email }}</td>
+     	<td>{{ $dt->akses }}</td>
+     	<td>
+     		<a href="{{ route('admin.user.edit',['id'=>$dt->id]) }}" 
+            class="btn btn-success btn-sm">
+     			<i class="fa fa-w fa-edit"></i>
+     		</a>
+
+            @if( $dt->id != Auth::id() )
+     		<button class="btn btn-danger btn-sm" type="button">
+     			<i class="fa fa-w fa-trash"></i>
+     		</button>
+            @endif
+
+     	</td>
+     </tr>
+     @endforeach
+</table>
+
+{{
+	$data->appends( request()->only('keyword') )
+	->links('vendor.pagination.bootstrap-4')
+}}
+
+
+@endsection
+
+
+
+
+
+
+
+
+
+
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['middleware'=>['auth']], function(){
+   Route::prefix('admin')->group(function(){
+		Route::get('/', function(){
+	       return view('admin.pages.dashboard');
+		})->name('admin.home');
+
+		Route::prefix('user')->group(function(){
+
+		   Route::get('/','UserController@daftar')->name('admin.user')->middleware('akses.admin');
+
+		   Route::get('/add','UserController@add')->name('admin.user.add')->middleware('akses.admin');
+		   Route::post('/add','UserController@save')->middleware('akses.admin');
+
+		   Route::get('/edit/{id}','UserController@edit')->name('admin.user.edit')
+		   			->middleware('akses.admin');
+		   Route::post('/edit/{id}','UserController@update')
+		   			->middleware('akses.admin');
+		   
+           Route::get('/setting','UserSettingController@form')->name('admin.user.setting');
+           Route::post('/setting','UserSettingController@update');
+		});
+	});
+});
+
+
+Auth::routes();
+
+Route::any('register',function(){ return abort(404); });
+
+
